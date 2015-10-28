528c9022d90e675f33dcac4f0409ddb8f934c00f
lotan1 <lotanbs@gmail.com>
2015-10-28 15:33:08 +0200

    next

--- server.py
+++ server.py
@@ -1,16 +1,31 @@
 import os
 import pathlib
 import calendar
-from bottle import route, run, static_file
+import datetime
+from bottle import route, run, static_file, template
 from datetime import datetime
 
-PORT = int(os.environ.get("PORT", 50))
+PORT = int(os.environ.get("PORT", 80))
 BASE_PATH = pathlib.Path(__file__).parent
-
 cal1 = calendar.HTMLCalendar(calendar.SUNDAY)
 year1 = datetime.now().year
 month1 = datetime.now().month
+
 print (year1, month1)
+
+def links(year, month):
+    if month > 1 :
+        monthP = month - 1
+    else:
+        monthP = 12,
+    if month < 11:
+        monthM = month + 1
+    else:
+        monthM = 1,
+    return {'monthP': monthP,
+            'monthM': monthM,
+           }
+
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
@@ -25,15 +40,11 @@ def show_page(year,month):
     year1 = int(year)
     month1 = int(month)
     print(year1,month1)
-    return cal1.formatmonth(year1, month1)
+    return template('calte2', calendar = cal1.formatmonth(year1,month1), monthP = monthP, monthM = monthM, year = year1)
 
 @route('/<year>')
 def show_page(year):
     year1 = int(year)
-    print(year1)
-    return cal1.formatyear(year1)
+    return template('calte', calendar = cal1.formatyear(year1))
 
-
-
-
-run(host='0.0.0.0', port=PORT)
+run(host='localhost', port=PORT)
---
+++ views/calte2.tpl
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+ {{! calendar}}
+  <a href="/{{year}}/{{! monthP}}"> "prev    " </a>
+  <a href="/{{year}}/{{! monthM}}"> "    next" </a>
+
+</body>
+</html>
